8a97f61d13e16859f23337cb54e41d5b328c5c7e
Fuji Pebri <pebri86@gmail.com>
2019-11-26 11:22:14 +0700

    manage project source structure

--- esplay-launcher/main/component.mk
+++ esplay-launcher/main/component.mk
@@ -2,7 +2,7 @@
 # "main" pseudo-component makefile.
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
-COMPONENT_ADD_INCLUDEDIRS := .
+COMPONENT_ADD_INCLUDEDIRS := include
 COMPONENTS_EXTRA_CLEAN := gfxTile.inc gfxTile.raw
 main.o: gfxTile.inc
 
--- esplay-sdk/esplay-components/esplay-ui/component.mk
+++ esplay-sdk/esplay-components/esplay-ui/component.mk
@@ -6,5 +6,5 @@
 # in the build directory. This behaviour is entirely configurable,
 # please read the ESP-IDF documents if you need to do this.
 #
-
-COMPONENT_ADD_INCLUDEDIRS := .
+COMPONENT_ADD_INCLUDEDIRS := include
+COMPONENT_SRCDIRS := src
